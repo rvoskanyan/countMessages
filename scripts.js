82f273baab4ecbd08a655c6c1aa8cee7cdb35203
rvoskanyan <54975205+rvoskanyan@users.noreply.github.com>
2019-10-04 15:48:28 +0300

    relise 2

--- scripts.js
+++ scripts.js
@@ -1,153 +1,354 @@
 class Messages {
 
-    getEmailsWithCountMessages(listMessagesJSON) {
-        function sortRules(a, b) {
-            if (a.email > b.email) return 1;
-            if (a.email === b.email) return 0;
-            if (a.email < b.email) return -1;
-        }
+    messagesJSON;
 
-        let sortedList = JSON.parse(this.sortListMessages(listMessagesJSON, sortRules));
-
-        let filteredList = [];
-
-        while(sortedList.length > 0) {
-            let emailFilter = sortedList[sortedList.length - 1].email;
-            let filteredListOnEmail = sortedList.filter(item => item.email === emailFilter);
-
-            filteredList.push({email: emailFilter, count: filteredListOnEmail.length});
-
-            sortedList.length -= filteredListOnEmail.length;
-        }
-
-        return JSON.stringify(filteredList);
+    constructor() {
+        this.messagesJSON = localStorage.getItem('messages');
     }
 
-    sortListMessages(listMessagesJSON, sortRules) {
-        let listMessages = JSON.parse(listMessagesJSON);
+    getListMessages() {
+        return this.messagesJSON;
+    }
 
-        return JSON.stringify(listMessages.sort(sortRules));
+    addMessage(message, id = false) {
+        let messages = JSON.parse(this.messagesJSON);
+        if(id) {
+            messages.find((item, index) => {
+                if(index === 0) return;
+                if(item.id == id) {
+                    messages[index].messages.push(JSON.parse(message));
+                    messages[index].messages[0]++;
+                    return true;
+                }
+            });
+        }
+        else {
+            messages.push(JSON.parse(message));
+            messages[0]++;
+        }
+
+        this.messagesJSON = JSON.stringify(messages);
+        this.reloadLocaleStorage();
+    }
+
+    deleteMessage(id_message, id_email) {
+        let messages = JSON.parse(this.messagesJSON);
+
+        messages.find((item, index) => {
+            if(index === 0) return;
+            let index_email = index;
+            if(item.id == id_email) {
+                item.messages.find((item, index) => {
+                    if(index === 0) return;
+                    if(item.id == id_message) {
+                        messages[index_email].messages.splice(index, 1);
+                        messages[index_email].messages[0]--;
+
+                        this.messagesJSON = JSON.stringify(messages);
+                        return true;
+                    }
+                });
+                return true;
+            }
+        });
+
+        this.reloadLocaleStorage();
+    }
+
+    deleteMessages(id_email) {
+        let messages = JSON.parse(this.messagesJSON);
+
+        messages.find((item, index) => {
+            if(index === 0) return;
+            if(item.id == id_email) {
+                messages.splice(index, 1);
+                messages[0]--;
+
+                this.messagesJSON = JSON.stringify(messages);
+                return true;
+            }
+        });
+
+        this.reloadLocaleStorage();
+    }
+
+    clearMessages() {
+        this.messagesJSON = JSON.stringify([1]);
+        this.reloadLocaleStorage();
+    }
+
+    reloadLocaleStorage() {
+        localStorage.setItem("messages", this.messagesJSON);
     }
 }
 
 class Executor {
 
-    messagesArrayJSON;
+    messagesJSON;
     objectMessages;
 
-
     constructor(objectMessages) {
-        this.messagesArrayJSON = localStorage.getItem('messages');
         this.objectMessages = objectMessages;
+        this.messagesJSON = this.objectMessages.getListMessages();
+        this.setState();
+    }
+
+    setState(id = false) {
+        let messages = JSON.parse(this.messagesJSON);
+        let result;
+
+        if(messages.length < 2) return this.renderTable();
+
+        if(id) {
+              messages.find(function(item, index){
+                  if(item.id == id) {
+                      messages[index].opened = !messages[index].opened;
+                      return true;
+                  }
+              });
+              result = messages;
+        }
+        else {
+            result = messages.map(function(item, index){
+                if(index === 0) return item;
+                item.opened = false;
+                return item;
+            });
+        }
+        this.messagesJSON = JSON.stringify(result);
         this.renderTable();
     }
 
-    sendMessage(email, message) {
-        let messagesArray = JSON.parse(this.messagesArrayJSON);
-        if(!messagesArray) messagesArray = [];
+    sendMessage(email, text) {
+        let messages = JSON.parse(this.messagesJSON);
 
-        messagesArray.push({email, message});
-        this.messagesArrayJSON = JSON.stringify(messagesArray);
-        localStorage.setItem('messages', this.messagesArrayJSON);
+        let result = messages.find((item, index) => {
+            if(index === 0) return;
+
+            if(item.email === email) {
+                let message = {
+                    id: item.messages[0],
+                    text,
+                };
+
+                item.messages[0]++;
+                messages[index].messages.push(message);
+
+                this.objectMessages.addMessage(JSON.stringify(message), messages[index].id);
+
+                return true;
+            }
+        });
+
+        if(!result) {
+            let message = {
+                id: messages[0],
+                email: email,
+                messages: [
+                    2,
+                    {id: 1, text},
+                ],
+            };
+
+            this.objectMessages.addMessage(JSON.stringify(message));
+            message.opened = false;
+            messages.push(message);
+            messages[0]++;
+        }
+
+        this.messagesJSON = JSON.stringify(messages);
+        this.renderTable();
+    }
+
+    deleteMessage(id_message, id_email) {
+        let messages = JSON.parse(this.messagesJSON);
+
+        messages.find((item, index) => {
+            if(index === 0) return;
+            let index_email = index;
+            if(item.id == id_email) {
+                item.messages.find((item, index) => {
+                    if(index === 0) return;
+                    if(item.id == id_message) {
+                        this.objectMessages.deleteMessage(id_message, id_email);
+                        messages[index_email].messages.splice(index, 1);
+                        messages[index_email].messages[0]--;
+
+                        this.messagesJSON = JSON.stringify(messages);
+                        return true;
+                    }
+                });
+                return true;
+            }
+        });
+
+        this.renderTable();
+    }
+
+    deleteMessages(id_email) {
+        let messages = JSON.parse(this.messagesJSON);
+
+        messages.find((item, index) => {
+            if(index === 0) return;
+            if(item.id == id_email) {
+                this.objectMessages.deleteMessages(id_email);
+                messages.splice(index, 1);
+                messages[0]--;
+
+                this.messagesJSON = JSON.stringify(messages);
+                return true;
+            }
+        });
+
+        this.renderTable();
+    }
+
+    clearMessages() {
+        this.objectMessages.clearMessages();
+        this.messagesJSON = JSON.stringify([1]);
         this.renderTable();
     }
 
     renderTable() {
         let contentTable = document.getElementById('listMessages');
-        let clearTable = document.createElement('tbody');
+        let clearTable = document.createElement('div');
+        let messages = JSON.parse(this.messagesJSON);
+
         clearTable.id = 'listMessages';
+        clearTable.className = 'table__body';
         contentTable.replaceWith(clearTable);
         contentTable = document.getElementById('listMessages');
 
-        if(!this.messagesArrayJSON) {
-            let tr = document.createElement('tr');
-            let tdEmail = document.createElement('td');
-            let tdCount = document.createElement('td');
+        if(messages.length < 2) {
+            let element = document.createElement('div');
+            let row = document.createElement('div');
+            let col = document.createElement('div');
 
-            tdEmail.append('Сообщений нет');
-            tdCount.append('');
+            element.className = 'element';
+            row.className = 'row';
+            col.className = 'col';
 
-            tr.append(tdEmail);
-            tr.append(tdCount);
-
-            contentTable.append(tr);
+            col.append('Сообщений нет');
+            row.append(col);
+            element.append(row);
+            contentTable.append(element);
             return;
         }
 
-        let listMessages = JSON.parse(this.objectMessages.getEmailsWithCountMessages(this.messagesArrayJSON));
-
-        for(let i = 0; i < listMessages.length; i++) {
-            let tr = document.createElement('tr');
-            let tdEmail = document.createElement('td');
-            let tdCount = document.createElement('td');
+        for(let i = 1; i < messages.length; i++) {
+            let element = document.createElement('div');
+            let row = document.createElement('div');
+            let emailCol = document.createElement('div');
+            let messagesCol = document.createElement('div');
+            let buttonCol = document.createElement('div');
+            let buttonDel = document.createElement('button');
             let contentCount;
 
-            if(listMessages[i].count == '1') {
-                let messagesArray = JSON.parse(this.messagesArrayJSON);
-                let filteredMessages = messagesArray.filter(function (item) {
-                    return item.email === listMessages[i].email;
-                });
-                contentCount = filteredMessages[0].message;
+            element.className = 'element';
+            row.className = 'row';
+            emailCol.className = 'col';
+            messagesCol.className = 'col';
+            buttonCol.classNmae = 'col del';
+
+            buttonDel.append('×');
+            buttonDel.setAttribute('onclick', `deleteMessagesByEmail('${messages[i].id}')`);
+            buttonCol.append(buttonDel);
+            emailCol.append(messages[i].email);
+            row.append(emailCol);
+
+            if(messages[i].messages.length < 3) {
+                contentCount = messages[i].messages[1].text;
             }
             else {
                 let button = document.createElement('button');
-                button.append(listMessages[i].count);
-                button.setAttribute('onClick', `showMessagesOnEmail('${listMessages[i].email}', 'emailNumber${i}')`);
+                if(messages[i].opened) {
+                    button.append(messages[i].messages.length - 1 + ' ▼');
+                }
+                else {
+                    button.append(messages[i].messages.length - 1 + ' ►');
+                }
+                button.setAttribute('onClick', `uncoverOnEmailId('${messages[i].id}')`);
                 contentCount = button;
             }
 
-            tdEmail.append(listMessages[i].email);
-            tdCount.append(contentCount);
-            tdCount.id = `emailNumber${i}`;
+            messagesCol.append(contentCount);
+            element.id = `emailId${messages[i].id}`;
 
-            tr.append(tdEmail);
-            tr.append(tdCount);
+            row.append(messagesCol);
+            row.append(buttonCol);
+            element.append(row);
 
-            contentTable.append(tr);
-        }
-    }
+            if(messages[i].opened && messages[i].messages.length > 2) {
+                let showedMessages = document.createElement("div");
+                let messagesArray = messages[i].messages;
 
-    showMessagesOnEmail(email, idElement) {
-        let messagesArray = JSON.parse(this.messagesArrayJSON);
+                showedMessages.className = 'showedMessages';
 
-        let filteredMessages = messagesArray.filter(function(item) {
-            return item.email === email;
-        });
+                for(let j = 1; j < messagesArray.length; j++) {
+                    let rowMessage = document.createElement("div");
+                    let colNumber = document.createElement("div");
+                    let colText = document.createElement("div");
+                    let colDel = document.createElement("div");
+                    let button = document.createElement("button");
 
-        let element = document.getElementById(idElement);
-        let clearElement = document.createElement('td');
-        clearElement.id = idElement;
-        element.replaceWith(clearElement);
-        element = document.getElementById(idElement);
+                    rowMessage.className = "rowMessage";
+                    colNumber.className = "col";
+                    colText.className = "col";
+                    colDel.className = "col del";
 
-        for(let i = 0; i < filteredMessages.length; i++) {
-            let str = i + 1 + ': ' + filteredMessages[i].message;
-            let br = document.createElement('br');
-            element.append(str);
-            element.append(br);
+                    button.append("×");
+                    button.setAttribute('onclick', `deleteMessageById('${messagesArray[j].id}', '${messages[i].id}')`);
+                    colDel.append(button);
+                    colText.append(messagesArray[j].text);
+                    colNumber.append(j);
+
+                    rowMessage.append(colNumber);
+                    rowMessage.append(colText);
+                    rowMessage.append(colDel);
+
+                    showedMessages.append(rowMessage);
+                }
+
+                element.append(showedMessages);
+            }
+
+            contentTable.append(element);
         }
     }
 }
 
-let messages = new Messages();
-let executor = new Executor(messages);
+let executor = new Executor(new Messages());
 
-function showMessagesOnEmail(email, idElement) {
-    executor.showMessagesOnEmail(email, idElement);
+function uncoverOnEmailId(id) {
+    executor.setState(id);
 }
 
 function send() {
     let email = document.getElementById('email').value;
-    let message = document.getElementById('message').value;
+    let text = document.getElementById('text').value;
 
-    executor.sendMessage(email, message);
+    let reg = /.+@.+\..+/i;
+    if(!reg.test(String(email).toLowerCase())) {
+        alert('Не верный E-mail');
+        return;
+    }
+
+    executor.sendMessage(email, text);
 
     document.getElementById('email').value = '';
-    document.getElementById('message').value = '';
+    document.getElementById('text').value = '';
+}
+
+function deleteMessageById(id_message, id_email) {
+    executor.deleteMessage(id_message, id_email);
+}
+
+function deleteMessagesByEmail(id_email) {
+    executor.deleteMessages(id_email);
 }
 
 function clearMessages() {
-    localStorage.clear();
-    executor.messagesArrayJSON = null;
-    executor.renderTable();
+    if(confirm('Удалить все сообщения?')) {
+        executor.clearMessages();
+    }
 }
